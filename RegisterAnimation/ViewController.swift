24cde53e282ba31a5b2420b092ac63895e9be65d
Alexander Frederik Vreeken <saf.vreeken@gmail.com>
2019-04-06 12:00:06 +0200

    Initial Commit

---
+++ RegisterAnimation/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RegisterAnimation
+//
+//  Created by Alexander Frederik Vreeken on 06/04/2019.
+//  Copyright © 2019 Alexander Frederik Vreeken. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
